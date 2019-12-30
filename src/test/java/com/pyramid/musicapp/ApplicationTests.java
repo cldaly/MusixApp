75e335196e924a5b83ca0da7a9387415f8d50311
cldaly <clayton.daly91@gmail.com>
2019-12-30 13:05:12 -0500

    initial commit

---
+++ src/test/java/com/pyramid/musicapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyramid.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
