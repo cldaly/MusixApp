5e7624b5b998ce3b1967e234dc3aa7c2d7423c6c
JordanAclarke <JordanAclarke@gmail.com>
2019-12-30 16:39:55 -0500

    Json Data showing

--- MusixAppUI/src/app/app.component.ts
+++ MusixAppUI/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MusicService } from './services/music.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private music:MusicService) {}
+  ngOnInit(): void {
+    this.music.getArtistByName("Cher");
+  }
   title = 'MusixAppUI';
 }
--- MusixAppUI/src/app/services/music.service.ts
+++ MusixAppUI/src/app/services/music.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { Artist } from '../models/artist';
 @Injectable({
   providedIn: 'root'
 })
 export class MusicService {
+  YOUR_API_KEY:string = "d02851a81c8e025c628c78519b0e0bd5";
+  artistName:Array<Artist> = [];
+  constructor(private HttpClient:HttpClient) { }
 
-  constructor() { }
+  getMusicByTrackName(trackName:string) {
+
+    return this.HttpClient.get('http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + trackName + '&api_key=' + this.YOUR_API_KEY + '&format=json');
+  }
+
+  getMusicByAlbumName(albumName:string) {
+    return this.HttpClient.get('http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + albumName + '&api_key=' + this.YOUR_API_KEY + '&format=json');
+  }
+
+  getArtistByName(artistName:string) {
+    this.HttpClient.get('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist='+ artistName + '&api_key=' + this.YOUR_API_KEY + '&format=json').subscribe(
+      data => {
+        for(let artist of data["results"]["artistmatches"]["artist"]) {
+          let eachArtist=new Artist();
+          eachArtist.artistName = artist["name"];
+          this.artistName.push(eachArtist);
+          console.log(eachArtist);
+        }       
+        console.log(this.artistName);
+      }
+    );
+  }
+  // One method pulling by track name
+  // Other pulling by album name
 }
