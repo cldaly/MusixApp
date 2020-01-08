872a986ac8b7fddae95f2882f4541f4f45fc9239
Fred <mtezera@gmail.com>
2020-01-08 10:27:11 -0500

    removed warnings from back end

--- src/main/java/com/pyramid/musicapp/model/Album.java
+++ src/main/java/com/pyramid/musicapp/model/Album.java
@@ -1,7 +1,6 @@
 package com.pyramid.musicapp.model;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
